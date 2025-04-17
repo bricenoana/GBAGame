0749204cf3e1c934b4c19ff617c331d1916cd470
bricenoana <bricenoana0@gmail.com>
2025-04-17 12:28:36 -0400

    Lose screen

--- loseScreen.h
+++ loseScreen.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 512 + 38400 = 38912
 //
-//	Time-stamp: 2025-04-06, 00:16:36
+//	Time-stamp: 2025-04-17, 12:27:55
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
