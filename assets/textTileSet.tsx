d4f8a844867d9873f24c4e86f52255d536e30162
bricenoana <bricenoana0@gmail.com>
2025-04-18 15:24:04 -0400

    text files and tiles

---
+++ assets/textTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="textTileSet" tilewidth="8" tileheight="8" tilecount="196" columns="28">
+ <image source="../../textTiles.png" width="224" height="60"/>
+</tileset>
---
+++ textTiles.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(textTiles)
+
+//======================================================================
+//
+//	textTiles, 224x64@4, 
+//	+ palette 256 entries, not compressed
+//	+ 224 tiles not compressed
+//	Total size: 512 + 7168 = 7680
+//
+//	Time-stamp: 2025-04-18, 15:06:33
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TEXTTILES_H
+#define GRIT_TEXTTILES_H
+
+#define textTilesTilesLen 7168
+extern const unsigned short textTilesTiles[3584];
+
+#define textTilesPalLen 512
+extern const unsigned short textTilesPal[256];
+
+#endif // GRIT_TEXTTILES_H
+
+//}}BLOCK(textTiles)
